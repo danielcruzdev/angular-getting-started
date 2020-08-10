29213ac48655f331a74172192de967fc03b164b7
Daniel Cruz <daniel.cruz@writesys.com.br>
2020-08-10 15:39:17 -0300

    using properties binding to show image

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
 })
 
 export class ProductListComponent {
-    pageTitle: "Product List"
+    pageTitle: "Product List";
+    imageWidth: number = 50;
+    imageMargin: number = 2;
     products: any[] = [
         {
             "productId": 1,
