26ffb5efed4bd4461af76a7d5f2791630353887c
Daniel Cruz <daniel.cruz@writesys.com.br>
2020-08-11 16:03:29 -0300

    doing more with components

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product'
 
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: "Product List";
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'Cart';
-    products: any[] = [
+    ngOnInit(): void {
+      console.log("In OnInit")
+    }
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
