fba640ee00afbf27cd3ccc4a867316778599343a
Daniel Cruz <daniel.cruz@writesys.com.br>
2020-08-12 16:06:03 -0300

    Create custom pipes and filter results

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -13,10 +13,15 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'Cart';
-    ngOnInit(): void {
-      console.log("In OnInit")
+    _listFilter: string;
+    get listFilter(): string {
+      return this._listFilter;
     }
+    set listFilter(value: string) {
+      this._listFilter = value;
+      this.filteredProducts = this.listFilter ? this.performFilter(this._listFilter) : this.products;
+    }
+    filteredProducts: IProduct[];
     products: IProduct[] = [
         {
             "productId": 1,
@@ -69,6 +74,22 @@ export class ProductListComponent implements OnInit {
             "imageUrl": "assets/images/xbox-controller.png"
           }
     ];
+
+    constructor() {
+      this.filteredProducts = this.products;
+      this.listFilter = 'cart'
+    }
+
+    performFilter(filterBy: string) : IProduct[] {
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.products.filter((product: IProduct) => 
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
+      )
+    }
+
+    ngOnInit(): void {
+      console.log("In OnInit")
+    }
 
     toggleImage(): void {
       this.showImage = !this.showImage;
