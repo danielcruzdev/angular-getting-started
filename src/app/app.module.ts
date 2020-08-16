7c53b4981fbedc5e3f6765e9b532adbb179670e4
Daniel Cruz <d.afc98@gmail.com>
2020-08-16 17:05:16 -0300

    Creating more modules to divide the declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,35 @@
 //#region MODULES
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { HttpClientModule } from '@angular/common/http';
 //#endregion
 
 //#region COMPONENTS
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { StarComponent } from "./shared/star-component";
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from "./home/welcome.component";
 //#endregion
 
 //#region GUARDS
-import { ProductDetailGuard } from "./products/product-detail.guard";
+import { ProductModule } from './products/product.module';
 //#endregion
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { 
-        path: 'product/:id', 
-        canActivate: [ProductDetailGuard], 
-        component: ProductDetailComponent 
-      },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' } 
-
-    ])
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
